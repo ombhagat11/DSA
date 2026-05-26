9c2cac29f67ef5e2e53950ac4e78b1f7b2b71db4
OM PRAKASH BHAGAT <104925493+ombhagat11@users.noreply.github.com>
2026-05-26 09:24:05 +0530

    Time: 7 ms (15.34%) | Memory: 8.7 MB (37.58%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if(x < 0) {
+            return false;
+        }
+
+        int original = x;
+
+        long long revNum = 0;
+
+        while(x > 0) {
+
+            int ld = x % 10;
+
+            revNum = (revNum * 10) + ld;
+
+            x = x / 10;
+        }
+
+        return revNum == original;
+    }
+};
