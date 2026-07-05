4287bfd611ad4a79057cf2fd581bfd8ac21927e9
OM PRAKASH BHAGAT <104925493+ombhagat11@users.noreply.github.com>
2026-07-05 12:15:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Set Bits/count-set-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int setBits(int n) {
+
+        int count = 0;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                count++;
+            }
+            n = n / 2;
+        }
+
+        return count;
+    }
+};
